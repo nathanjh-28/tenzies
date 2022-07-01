729702bb3cb9d9ab66978203cee62dc25d1459b2
nathanjh-28 <nathan@nathanjharris.com>
2022-06-30 21:55:13 -0700

    added dice boxes and styling

--- src/App.js
+++ src/App.js
@@ -1,10 +1,31 @@
 import React from 'react'
 import './style.css'
+import Die from './Die'
 
 export default function App() {
+
+    function randomDie() {
+
+        const num = Math.floor(Math.random() * 6)
+        return num > 0 ? num : randomDie()
+    }
+
     return (
         <main>
-            <div></div>
+            <div className="die-container">
+
+                <Die value={randomDie()} />
+                <Die value={randomDie()} />
+                <Die value={randomDie()} />
+                <Die value={randomDie()} />
+                <Die value={randomDie()} />
+                <Die value={randomDie()} />
+                <Die value={randomDie()} />
+                <Die value={randomDie()} />
+                <Die value={randomDie()} />
+                <Die value={randomDie()} />
+            </div>
+
         </main>
     )
 }
---
+++ src/Die.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+export default function Die(props) {
+    return (
+        <div className="die">
+            <p>
+                {props.value}
+            </p>
+        </div>
+    )
+}
